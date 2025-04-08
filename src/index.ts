c922d92a7cfa4b268d21d0e27ee516a1544c6c6b
frontfeodot <frontfeodot@gmail.com>
2025-04-08 19:21:10 +0300

    fixing cors issues

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use( cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
-app.use(cors(/* corsOptions */));
 
 app.use("/auth", loginRoute);
 
